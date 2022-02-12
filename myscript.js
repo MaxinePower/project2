e50736ef929efcf407def413a14eaf164bcf3754
Sousa89 <kevin.de.sousa89@gmail.com>
2022-02-11 19:06:16 -0500

    farfenugen

--- myscript.js
+++ myscript.js
@@ -1,11 +1,19 @@
- const apiCall = {};
-        apiCall.apiURL = 'https://api.themoviedb.org/3/discover/movie';
-        apiCall.apiKey = '4313ef3612655092b3c706f021f3d6bc';
+ 
+//  name spacing
+ const movieApp = {};
+       
+// form element in variable
+movie.movieApp.form = document.querySelector(`form`);
+// method for even listeners
 
+//  url and api key variables
+ movieApp.apiURL = 'https://api.themoviedb.org/3/discover/movie';
+        movieApp.apiKey = '4313ef3612655092b3c706f021f3d6bc';
 
-        apiCall.getData = () => {
+// make method getData
+        movieApp.getData = () => {
             const url =
-                `${apiCall.apiURL}?api_key=${apiCall.apiKey}&vote_average.gte=2&vote_average.lte=5`;
+                `${movieApp.apiURL}?api_key=${movieApp.apiKey}&vote_average.gte=2&vote_average.lte=5`;
             // url.movie = new URLSearchParams({
             //     query: 'cat',
             //     api_key: apiCall.apiKey
