75db59dd3d45f1c099619d9b77bf7eb65e73bf3a
Maxine <maxine@power.sh>
2022-02-15 20:40:34 -0500

    reworked genre dropdown

--- myscript.js
+++ myscript.js
@@ -6,12 +6,30 @@ const movieApp = {};
     // movieApp.apiKey = '4313ef3612655092b3c706f021f3d6bc';
 
 
-    const movieAppUrl = new URL("https://api.themoviedb.org/3/discover/movie")
-    const apiKey = '4313ef3612655092b3c706f021f3d6bc';
+const movieAppUrl = new URL("https://api.themoviedb.org/3/discover/movie");
+const apiKey = '4313ef3612655092b3c706f021f3d6bc';
 
-// movieApp.genreList = {
-    
-// }
+// making a list object for genres
+movieApp.genreList = {};
+
+// making method to create genre dropdown list
+movieApp.genreOptions = function() {
+    fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=4313ef3612655092b3c706f021f3d6bc&language=en-US').then(function(res) {
+        return res.json();
+    }).then(function(jsonRes) {
+        jsonRes.genres.forEach(function(genre) {
+            // creating dropdown menu
+            genreDropdown = document.getElementById('genreSelect');
+            genreDropdownEntry = document.createElement('option');
+            genreDropdownEntry.value = genre.id;
+            genreDropdownEntry.textContent = genre.name;
+            genreDropdown.append(genreDropdownEntry);
+            // this is adding it to a variable for later use
+            movieApp.genreList[genre.id] = genre.name;
+            console.log(movieApp.genreList);
+        });
+    });
+};
 
 
     // form element in variable
@@ -21,7 +39,7 @@ const movieApp = {};
         movieApp.form.addEventListener('submit', function(e) {
             e.preventDefault();
 
-            const genre = document.getElementById('genre').value;
+            const genre = document.getElementById('genreSelect').value;
             movieApp.getData(genre);
             // to reset to random se
             // movieApp.form.reset();
@@ -61,6 +79,7 @@ const movieApp = {};
                 const voteAvg = movieArray.vote_average;
                 const movieTitle = movieArray.title;
                 const releaseDate = movieArray.release_date;
+                // used in a for each to name each genre num before display
                 const genreId = movieArray.genre_ids;
                 const language = movieArray.original_language;
 
@@ -72,6 +91,7 @@ const movieApp = {};
 
                 // const genre = document.querySelector(`.genre`);
                 // genre.innerHTML = 
+                // foreach loop that goes through genre_ids then puts that indexNumber in like movieApp.genreList[indexNumber]
 
                 const rating = document.querySelector(`.rating`);
                 rating.innerHTML = `Rating: ${voteAvg}/10`
@@ -104,6 +124,7 @@ const movieApp = {};
     };
 
     movieApp.init = function () {
+        movieApp.genreOptions();
         movieApp.formSubmit();
     }
     
