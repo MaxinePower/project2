ed93928868a6d5bb000831bc1ee68f615daca00a
Maxine <maxine@power.sh>
2022-02-18 11:30:43 -0500

    added comments and removed double spacing from script

--- myscript.js
+++ myscript.js
@@ -1,11 +1,6 @@
 // name spacing
 const movieApp = {};
 
-    // url and apikey variables
-    // movieApp.apiURL = 'https://api.themoviedb.org/3/discover/movie';
-    // movieApp.apiKey = '4313ef3612655092b3c706f021f3d6bc';
-
-
 const movieAppUrl = new URL("https://api.themoviedb.org/3/discover/movie");
 const apiKey = '4313ef3612655092b3c706f021f3d6bc';
 
@@ -32,56 +27,56 @@ movieApp.genreOptions = function() {
 
 console.log(movieApp.genreList);
 
-    // form element in variable
+// grabbing form element in variable
 movieApp.form = document.querySelector('form');
-// method for event listener
+// method for event listener on the form
 movieApp.formSubmit = function() {
     movieApp.form.addEventListener('submit', function(e) {
+        // preventing automatic refresh
         e.preventDefault();
     
-
         const genre = document.getElementById('genreSelect').value;
         movieApp.getData(genre);
-        // to reset to random se
+        // to reset to random selection, but its more convinient in the app not to
         // movieApp.form.reset();
+
         // console.log(movieApp.form[1]);
+
         e.target.blur();
     });
-
 };
 
+// generating a random array index number
 const randomNumber = (array)=>{
     const arrayIndex = Math.floor(Math.random() * array.length);
-    return array[arrayIndex]
+    return array[arrayIndex];
 }
 
-// make method getData()
+// making method getData()
 movieApp.getData = (genreID) => {
+    // search parameters
     movieAppUrl.search = new URLSearchParams({
         api_key: apiKey,
         'vote_average.gte': 2,
         'vote_average.lte': 5,
         with_genres: genreID,
         with_original_language: 'en',
-
     })
 
+    // fetching a movie with the users inputed query
     fetch(movieAppUrl)
         .then(function (data) {
             return data.json();
         })
         .then(function (response) {
-
             const movieArray = randomNumber(response.results);
-            console.log(movieArray);
-        
-
             // console.log(response.results[randomNumber(response.results)] );
+
             const overview = movieArray.overview;
             const poster = movieArray.poster_path;
             const voteAvg = movieArray.vote_average;
             const movieTitle = movieArray.title;
-            const releaseDate = movieArray.release_date;
+            const releaseDate = movieArray.release_date
             // used in a for each to name each genre num before display
             const genreId = movieArray.genre_ids;
             // console.log(genreId);
@@ -89,17 +84,15 @@ movieApp.getData = (genreID) => {
 
             const fullPoster = `https://image.tmdb.org/t/p/original/${poster}`;
 
-            
-
-
-            // grabbing main to append generated movie to
+            // grabbing movieSelectionContainer to append generated movie to, and clearing it before we do
             const movieSelectionContainer = document.querySelector('.movieSectionContainer');
-      
             movieSelectionContainer.innerHTML="";
+
             // creating section element for the generated movie
             const generatedMovie = document.createElement('section');
-            // generatedMovie.innerHTML = '';
             generatedMovie.classList.add('generatedMovie');
+
+            // ---------MOVIE POSTER---------
 
             // create div.imgContainer with an img.poster in it
             const imgContainer = document.createElement('div');
@@ -108,12 +101,13 @@ movieApp.getData = (genreID) => {
             const posterElement = document.createElement('img');
             posterElement.classList.add('poster');
             posterElement.alt = 'movie poster';
-
-            // appending poster stuff into the section
-            generatedMovie.appendChild(imgContainer).appendChild(posterElement);
-            // this is up here for now 
-            movieSelectionContainer.append(generatedMovie);
             
+            // putting img src in the poster
+            const posterImg = document.querySelector(`.poster`)
+            posterImg.setAttribute('src', fullPoster);
+
+            // --------MOVIE INFO DIV STUFF---------
+
             // create div.movieInfo with an h3, div.genreRating, div.dateLanguage, p.overview
             const movieInfo = document.createElement('div');
             movieInfo.classList.add('movieInfo');
@@ -126,19 +120,17 @@ movieApp.getData = (genreID) => {
             const genreRating = document.createElement(`div`);
             genreRating.classList.add('genreRating');
 
-            
             // created and filled p tag with the movie rating
             const rating = document.createElement(`p`);
             rating.classList.add(`rating`);
             rating.innerHTML = `Rating: ${voteAvg}/10`;
 
-            
+            // trying to display the genre
             const genre = document.createElement(`p`);
             genre.classList.add(`genre`);
             // genre.innerHTML = genreList[];
             // foreach loop that goes through genre_ids then puts that indexNumber in like movieApp.genreList[indexNumber]
             
-
             // -------DATE LANGUAGE CONTAINER----
             
             // created div that holds date and language p tags and content
@@ -156,31 +148,36 @@ movieApp.getData = (genreID) => {
             releaseDateP.textContent = `Release Date: ${releaseDate}`;
 
             // created p tag for movie overview (with content)  
-            
             const description = document.createElement(`p`);
             description.classList.add(`overview`);
             description.textContent = overview;
             
-    
-            movieInfo.append(genreRating, dateLanguage,description);
+            // -------APPENDING STUFF-------
+
+            // appending poster stuff into the section
+            generatedMovie.appendChild(imgContainer).appendChild(posterElement);
+
+            // appending rating into rating div
             genreRating.appendChild(rating);
+            // appending content into dateLanguage
             dateLanguage.append(languageP, releaseDateP);
-            
-            
-            // instead of finding these elements, maybe create them????
-            
-            const posterImg = document.querySelector(`.poster`)
-            posterImg.setAttribute('src', fullPoster);
-            
+
+            // appending all of the elements we created into movieInfo
+            movieInfo.append(genreRating, dateLanguage, description);
+
+            // appending movieInfo div into generatedMovie section
             generatedMovie.appendChild(movieInfo);
-            // appending into the end of the main
-            // main.append(generatedMovie);
+
+            // appending section onto the page
+            movieSelectionContainer.append(generatedMovie);
         });
     };
-    
-    movieApp.init = function () {
-        movieApp.genreOptions();
-        movieApp.formSubmit();
-    }
-    
+
+// initialization method to start the page doing things
+movieApp.init = function () {
+    movieApp.genreOptions();
+    movieApp.formSubmit();
+}
+
+// calling the initialization method
 movieApp.init();
